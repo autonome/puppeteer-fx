de61a22f4925a0ec08b8424a516e9272c906fe23
TechQuery <shiy007@qq.com>
2018-04-06 04:57:42 +0800

    [ Add ]  API document based on JSDoc & DocDash

--- source/Browser.js
+++ source/Browser.js
@@ -3,6 +3,12 @@
 const EventEmitter = require('events'), Page = require('./Page');
 
 
+/**
+ * A Browser is created when Puppeteer connects to a Firefox instance
+ *
+ * @class
+ * @extends EventEmitter
+ */
 class Browser extends EventEmitter {
 
     constructor({browser, tab}) {
@@ -14,11 +20,21 @@ class Browser extends EventEmitter {
         this._tab = tab;
     }
 
+    /**
+     * @return {Promise<Page>}
+     */
     newPage() {
 
         return  new Page({tab:  this._tab});
     }
 
+    /**
+     * Closes Firefox and all of its pages (if any were opened).
+     *
+     * The Browser object itself is considered to be disposed and cannot be used anymore.
+     *
+     * @return {Promise}
+     */
     close() {
 
         return  this._browser.close();
--- source/Page.js
+++ source/Page.js
@@ -3,6 +3,14 @@
 const EventEmitter = require('events');
 
 
+/**
+ * Page provides methods to interact with a single tab in Firefox.
+ *
+ * One Browser instance might have multiple Page instances.
+ *
+ * @class
+ * @extends EventEmitter
+ */
 class Page extends EventEmitter {
 
     constructor({tab}) {
@@ -38,14 +46,27 @@ class Page extends EventEmitter {
         return inflated;
     }
 
+    /**
+     * @param {string} [url='about:blank'] URL to navigate page to.
+     *                                     The url should include scheme, e.g. https://.
+     * @return {Promise}
+     */
     goto(url) {
 
         return  this._tab.navigateTo( url );
     }
 
-    async evaluate(arg) {
+    /**
+     * @param {function|string} expression - Function or Expression to be evaluated in the page context
+     * @param {Serializable}    parameter  - Arguments to pass to the function
+     *
+     * @return {Promise<Serializable>} Promise which resolves to the value of `expression`
+     */
+    async evaluate(expression, ...parameter) {
 
-        return  Page.inflateGrip(await this._tab.console.evaluateJS( arg ));
+        return Page.inflateGrip(
+            await this._tab.console.evaluateJS(expression, ...parameter)
+        );
     }
 }
 
